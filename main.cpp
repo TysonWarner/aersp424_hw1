73cabd38bbacf8e99d2596da51316941ce85e5eb
TysonWarner <tysonnwarner@gmail.com>
2023-10-02 19:19:51 -0400

    Added some comments in main.cpp and did question 4.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,50 @@
 #include <iostream>
+#include <vector>
 #include "print.h"
 #include "RollRate.h"
 
 using namespace std;
 
+double dotproduct(vector<double> w, vector<double> x)
+{
+	double z = 0;
+	for (int i = 0; i < w.size(); i++)
+	{
+		z = z + w[i] * x[i];
+	}
+	return z;
+}
 int main()
 {
+#if 0
+	// I used an int "question" to adjust the aileron deflection angle (delta) that was being used.
+	// I made a separate .cpp and .h file (RollRate.cpp/.h) to get the results for questions 1-3
+	// There are separate notes in those files as well
+
+	// QUESTION 1 
 	int question = 1;
-	//cout << RollRate(question) << endl;
+	cout << RollRate(question) << endl;
 
+	// QUESTION 2
 	question = 2;
-	//cout << "\n\n\n" << RollRate(question) << endl;
+	cout << "\n\n\n" << RollRate(question) << endl;
 
+	// QUESTION 3
 	question = 3;
 	cout << "\n\n\n" << RollRate(question) << endl;
+
+	// QUESTION 4
+	vector<double> w, x;
+	w = { 0.0001,0.0001,0.0001 };
+	x = { 124,31.89,20.945 };
+	double z = dotproduct(w, x);
+	cout << z << endl;
+#endif
+
+
+
+
+
+
 	return 0;
 }
