73495847c0e75bb4554a99befdccc6cbb7141886
TysonWarner <tysonnwarner@gmail.com>
2023-10-02 23:57:01 -0400

    Done question 7.

--- main.cpp
+++ main.cpp
@@ -20,10 +20,32 @@ double sigmoid(double z)
 	double sigma = 1 / (1 + exp(-z));
 	return sigma;
 }
-double gradientcost(double sigma)
+double gradient_sigmoid(double sigma)
 {
 	double sigmaprime = sigma * (1 - sigma);
 	return sigmaprime;
+}
+double gradient_cost(double y_predict, double y)
+{
+	double dC = 2 * (y_predict - y);
+	return dC;
+}
+vector<double> gradient_weights(double sigma, double sigmaprime, double y, vector<double> x)
+{
+	vector<double> dw(x.size());
+	for (int i = 0; i < x.size(); i++)
+	{
+		dw[i] = (2 * (sigma - y)) * (sigmaprime) * x[i];
+	}
+	return dw;
+}
+vector<double> update_weights(double alpha, vector<double> w, vector<double> dw)
+{
+	for (int i = 0; i < w.size(); i++)
+	{
+		w[i] = w[i] - alpha * dw[i];
+	}
+	return w;
 }
 int main()
 {
@@ -43,7 +65,7 @@ int main()
 	// QUESTION 3
 	question = 3;
 	cout << "\n\n\n" << RollRate(question) << endl;
-
+#endif
 	// QUESTION 4
 	vector<double> w, x;
 	w = { 0.0001,0.0001,0.0001 };
@@ -52,9 +74,22 @@ int main()
 
 	// QUESTION 5
 	double sigma = sigmoid(z);
-	double sigmaprime = gradientcost(sigma);
-#endif
-	// QUIESTION 6
+	double sigmaprime = gradient_sigmoid(sigma);
+
+	// QUESTION 6
+	double y_predict = 0.504421, y = 1.0;
+	double dC = gradient_cost(y_predict, y);
+
+	// QUESTION 7
+	double alpha = 0.001;
+	vector<double> dw = gradient_weights(sigma, sigmaprime, y, x);
+	w = update_weights(alpha, w, dw);
+
+	// QUESTION 8
+
+
+
+
 
 
 
